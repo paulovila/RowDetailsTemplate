cc4041d901e1378da07bdbd4eb451eb8970a69fd
Paulo Vilá <paulovila@live.com>
2023-01-19 17:53:03 +0000

    Workaround

--- borra2/borra2.Shared/MainPage.xaml.cs
+++ borra2/borra2.Shared/MainPage.xaml.cs
@@ -13,22 +13,9 @@ namespace borra2
         public MainPage()
         {
             this.InitializeComponent();
-            As = new List<A> {
-                new A { DocumentNumber = "d1", RevisionNumber = "101", Bs = new List<B>() },
-                new A { DocumentNumber = "d2", RevisionNumber = "102", Bs = new List<B>() }};
-            AddCommand = new Command(OnAdd);
+            V = new Vm();
         }
-
-        private void OnAdd()
-        {
-            var a1 = As[0];
-            a1.Bs.Add(new B { Validation = "Yes" });
-            a1.Notify("Bs");
-        }
-
-        public ICommand AddCommand { get; set; }
-
-        public List<A> As { get; set; }
+        public Vm V { get; }
     }
 
     public class Command : ICommand
@@ -40,17 +27,45 @@ namespace borra2
         public void Execute(object parameter) => _action();
     }
 
+    public class Vm : INotifyPropertyChanged
+    {
+        public Vm()
+        {
+            As = new List<A> {
+                new A { DocumentNumber = "d1", RevisionNumber = "101", Bs = new List<B>() },
+                new A { DocumentNumber = "d2", RevisionNumber = "102", Bs = new List<B>() }};
+            AddCommand = new Command(OnAdd);
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void Notify(string s) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(s));
+
+        private void OnAdd()
+        {
+            var a1 = As[0];
+            a1.Bs.Add(new B { Validation = "Row detail 1" });
+            a1.Bs.Add(new B { Validation = "Row detail 2" });
+            a1.Notify("Bs");
+            Notify("AggregatedValidations");
+        }
+
+        public ICommand AddCommand { get; set; }
+        public IEnumerable<string> AggregatedValidations { get => As
+                .SelectMany(w => w.Bs)
+                .Select(w => w.Validation)
+                .GroupBy(w=>w)
+                .Select(w=>$"({w.Count()}) {w.Key}")
+                .ToList(); }
+        public List<A> As { get; set; }
+    }
+
     public class A : INotifyPropertyChanged
     {
-        public void Notify(string s)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(s));
-        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void Notify(string s) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(s));
         public List<B> Bs { get; set; }
         public string DocumentNumber { get; set; }
         public string RevisionNumber { get; set; }
 
-        public event PropertyChangedEventHandler PropertyChanged;
     }
     public class B { public string Validation { get; set; } }
     public class NullOrNoneToVisibilityConverter : IValueConverter
@@ -63,11 +78,11 @@ namespace borra2
 
             if (value == null || enumerable?.Any() == false)
             {
-                return VisibleWhenNullOrNone ? Visibility.Visible : Visibility.Collapsed;
+                return Visibility.Collapsed;
             }
             else
             {
-                return VisibleWhenNullOrNone ? Visibility.Collapsed : Visibility.Visible;
+                return Visibility.Visible;
             }
         }
 
